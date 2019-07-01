407f52fbf24e8d39f747ed36f7317093a7945b45
kotoyuuko <kokoro@core.moe>
2019-07-01 17:24:15 +0800

    remove unused auth routes

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,12 @@
 <?php
 
-Auth::routes();
+Route::get('/login', 'Auth\\LoginController@showLoginForm')->name('login');
+Route::post('/login', 'Auth\\LoginController@login');
+Route::get('/register', 'Auth\\RegisterController@showRegistrationForm')->name('register');
+Route::post('/register', 'Auth\\RegisterController@register');
+Route::post('/logout', 'Auth\\LoginController@logout')->name('logout');
 
-Route::group(['middleware' => ['auth', 'verified']], function () {
+Route::group(['middleware' => ['auth']], function () {
     Route::get('/', 'PagesController@root')->name('root');
     Route::get('/comic/{name}', 'ComicsController@viewer')->name('comic.viewer');
     Route::get('/upload', 'PagesController@upload')->name('upload');
