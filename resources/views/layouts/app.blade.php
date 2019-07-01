dab7e97085e7a0f590bbd9297fde41173d013f9b
kotoyuuko <kokoro@core.moe>
2019-07-01 20:52:57 +0800

    fix scale problem on mobile browsers

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="viewport" content="width=device-width, initial-scale=0.6, maximum-scale=0.6, user-scalable=0">
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <title>@yield('title', 'komic')</title>
   <link href="{{ mix('css/app.css') }}" rel="stylesheet">
