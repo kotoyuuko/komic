cbdf7e3b33738f948c6d27cbb418d7024066c668
kotoyuuko <kokoro@core.moe>
2019-07-01 16:06:41 +0800

    add auth framework

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+Auth::routes();
 Route::get('/', 'PagesController@root')->name('root');
 Route::get('/comic', 'ComicsController@comics')->name('comic.list');
 Route::get('/comic/{name}', 'ComicsController@viewer')->name('comic.viewer');
