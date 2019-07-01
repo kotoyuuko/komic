2dd5551c9e4fabf66f943b0c3462a86ddd15224b
kotoyuuko <kokoro@core.moe>
2019-07-01 17:26:33 +0800

    remove unused code in app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,8 +3,6 @@ window.Vue = require('vue');
 const files = require.context('./components/', true, /\.vue$/i);
 files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
 
-// Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-
 const app = new Vue({
   el: '#app',
 });
